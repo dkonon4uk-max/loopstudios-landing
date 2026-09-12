f3588445df81b5c505e9b8793489a9565abcfeef
dkonon4uk-max <d.konon4uk@gmail.com>
2026-09-12 16:53:43 +0300

    upd readme

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const menuList = document.querySelector('body header ul')
 burgerBtn.addEventListener('click', () => {
     const isOpen = menuList.classList.toggle('open')
 
-
     logo.style.zIndex = isOpen ? '999' : ''
     burgerBtn.style.zIndex = isOpen ? '999' : ''
     burgerBtn.classList.toggle('close', isOpen)
